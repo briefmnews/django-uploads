d0d2a67cbf34262191b9cb7e2568ca8c75ee5972
Thomas <tcarbillet@brief.me>
2023-05-09 12:20:42 +0200

    chore(setup.py): update config

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup
 
+from uploads import __version__
+
 from os import path
 
 this_directory = path.abspath(path.dirname(__file__))
@@ -8,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="django-uploads",
-    version="0.2.0",
+    version=__version__,
     description="Django simple file uploader app.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -19,22 +21,20 @@ setup(
     packages=["uploads", "uploads.signals", "uploads.migrations"],
     python_requires=">=3.6",
     install_requires=[
-        "Django>=2",
+        "Django>=3",
     ],
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
-        "Framework :: Django :: 2.0",
-        "Framework :: Django :: 2.1",
-        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     include_package_data=True,
     zip_safe=False,
