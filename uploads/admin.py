b44626443c17a7350468740218cc0b0cd3e58041
Michael <mlemay@brief.me>
2025-06-02 17:56:29 +0200

    feat(django-uploads): Add warning for image sizes

--- uploads/admin.py
+++ uploads/admin.py
@@ -1,14 +1,50 @@
 from django.conf import settings
 from django.contrib import admin
 from django.utils.html import mark_safe
+from django.contrib import messages
 
 from .models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
-    list_display = ("__str__", "description", "file_absolute_url", "updated")
+    list_display = ("__str__", "description", "get_file_size", "file_absolute_url", "updated")
     readonly_fields = ("updated", "created")
-    search_fields = ("name", "description")
+    ordering = ("-updated",)
+    search_fields = ("name", "description", "get_file_size")
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        if obj and obj.file.size >= settings.UPLOADS_WARNING_SIZE and self.is_image(obj):
+            if not hasattr(request, '_document_size_warning'):
+                messages.warning(
+                    request,
+                    f"Attention : Cette image fait {self.get_file_size(obj)}. Il est recommandé de ne pas dépasser {int(settings.UPLOADS_WARNING_SIZE / 1024.0)} Ko."
+                )
+                request._document_size_warning = True
+        return form
+
+    @admin.display(
+        description="Taille du fichier",
+    )
+    def get_file_size(self, obj):
+        size = obj.file.size
+        warning = ""
+        if size >= settings.UPLOADS_WARNING_SIZE and self.is_image(obj): warning = "⚠️"
+
+        if size < 512000:
+            size = size / 1024.0
+            ext = 'Ko'
+        elif size < 4194304000:
+            size = size / 1048576.0
+            ext = 'Mo'
+        else:
+            size = size / 1073741824.0
+            ext = 'Go'
+
+        return f"{str(round(size, 2))} {ext} {warning}"
+    
+    def is_image(self, obj):
+        return obj.file.name.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.webp'))
 
     def file_absolute_url(self, obj):
         url = f"{settings.SITE_DOMAIN}{obj.file.url}"
