93d299f086fbe81fd44498b12140e258aa55ed9e
Michael <mlemay@brief.me>
2025-07-07 15:46:35 +0200

    refactor(django-uploads): Change warning position from message.warning banner to form field

--- uploads/admin.py
+++ uploads/admin.py
@@ -1,27 +1,33 @@
 from django.conf import settings
 from django.contrib import admin
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.utils.html import mark_safe
-from django.contrib import messages
 
 from .models import Document
 
 
 class DocumentAdmin(admin.ModelAdmin):
     list_display = ("__str__", "description", "file_size", "file_absolute_url", "updated")
-    readonly_fields = ("updated", "created")
+    readonly_fields = ("file_size_display", "updated", "created")
     ordering = ("-updated",)
     search_fields = ("name", "description")
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'description', 'file', 'file_size_display', 'updated', 'created')
+        }),
+    )
 
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
+    def file_size_display(self, obj):
         if obj and obj.file.size >= settings.UPLOADS_WARNING_SIZE and obj.is_image():
-            if not hasattr(request, '_document_size_warning'):
-                messages.warning(
-                    request,
-                    f"Attention : Cette image fait {obj.get_file_size()}. Il est recommandé de ne pas dépasser {int(settings.UPLOADS_WARNING_SIZE / 1024.0)} Ko."
-                )
-                request._document_size_warning = True
-        return form
+            return (
+                f"<div style='color:black;background:#fff3cd;padding:8px;'>"
+                f"⚠️ Attention : Cette image fait {obj.get_file_size()}. Il est recommandé de ne pas dépasser {int(settings.UPLOADS_WARNING_SIZE / 1024.0)} Ko."
+                f"</div>"
+            )
+        return f"{obj.get_file_size()}"
+    file_size_display = mark_safe(file_size_display)
+    file_size_display.short_description = "Taille du fichier"
 
     @admin.display(description="Taille du fichier")
     def file_size(self, obj):
